dfba39e94f47747dd25cec965950315367667b59
Avetis <87875341+AvetisHarutyunyan@users.noreply.github.com>
2022-10-24 13:38:24 +0400

    Add files via upload

---
+++ sss/js/scroll.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".header__section");
+    const navPosition = document.querySelector(".navside");
+    const toUpArrow = document.getElementById("toTopBtn");
+    
+    navbar.classList.toggle("navHeight", window.scrollY > 60);
+    navPosition.classList.toggle("navHeight", window.scrollY > 60);
+    toUpArrow.classList.toggle("toUpBtnActive", window.scrollY > 600);
+});
