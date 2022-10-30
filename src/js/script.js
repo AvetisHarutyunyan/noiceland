4f89bbe8629689d3bf83123643c712d1f848c67a
Avetis Harutyunyan <avetis.harutyunyan.98@gmail.com>
2022-10-30 21:56:28 +0400

    update 30.10.2022

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
     const modToggle = document.querySelectorAll(".darkThemeBtn");
-    const darkMode = document.querySelector("body");
+    const darkMode = document.body;
     const toggleText = document.querySelector(".darkTheme_text");
 
+    let local = localStorage.getItem("dark");
+
     modToggle.forEach(item => {
+
+        if (local === "enabled") {
+            item.classList.add("toggleActive");
+            darkMode.classList.add("darkMode");
+            toggleText.textContent = "Light Mode";
+        } else {
+            item.classList.remove("toggleActive");
+            darkMode.classList.remove("darkMode");
+            toggleText.textContent = "Dark Mode";
+        }
+
         item.addEventListener("click", () => {
             if (!item.classList.contains("toggleActive")) {
                 item.classList.add("toggleActive");
@@ -14,15 +27,27 @@ window.addEventListener("DOMContentLoaded", () => {
                 darkMode.classList.remove("darkMode");
                 toggleText.textContent = "Dark Mode";
             }
+
+            local = localStorage.getItem("dark");
+            if (local === "disabled") {
+                item.classList.add("toggleActive");
+                darkMode.classList.add("darkMode");
+                toggleText.textContent = "Light Mode";
+
+                localStorage.setItem("dark", "enabled");
+            } else {
+                item.classList.remove("toggleActive");
+                darkMode.classList.remove("darkMode");
+                toggleText.textContent = "Dark Mode";
+
+                localStorage.setItem("dark", "disabled");
+            }
+
         });
     });
 
-    const years = document.getElementById("CopyrightYear");
-    years.textContent = new Date().getFullYear();
-
     const nav = document.querySelector(".navside");
     const burger = document.querySelector(".navBurger");
-    
 
     burger.addEventListener("click", () => {
         if (!burger.classList.contains("close")) {
@@ -33,4 +58,7 @@ window.addEventListener("DOMContentLoaded", () => {
             nav.classList.remove("navActive");
         }
     });
+
+    const years = document.getElementById("CopyrightYear");
+    years.textContent = new Date().getFullYear();
 });
